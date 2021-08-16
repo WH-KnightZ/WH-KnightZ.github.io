e35c580dfafa3743ca6bae0041d9ae99066ebe3e
dealer <khanh.nguyen@boot.ai>
2021-08-16 10:55:20 +0700

    add

--- src/components/table/index.tsx
+++
@@ -1,5 +0,0 @@
-export { default as ListToolbar } from './listToolbar/ListToolbar';
-export { default as MoreMenu } from './moreMenu/MoreMenu';
-export { default as SearchNotFound } from './searchNotFound/SearchNotFound';
-export { default as DataTable } from './dataTable/DataTable';
-export { default as Row } from './row/Row';
--- src/extensions/constants.ts
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_PAGE_SIZE = 10;
-export const DEFAULT_AVATAR = '/static/mock-images/avatars/avatar_default.jpg';
