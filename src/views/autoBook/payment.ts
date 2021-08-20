cffe2905ae9dd2267908f0b83e44882203c82665
dealer <khanh.nguyen@boot.ai>
2021-08-20 14:01:28 +0700

    update price

--- src/views/autoBook/payment.ts
+++ src/views/autoBook/payment.ts
@@ -53,7 +53,7 @@ const confirmPayment = ({ name, email, paymentId, clientSecret, paymentMethod, c
 const paymentIntent = (payment: PaymentType) => {
   const { price, appointmentId, appointmentName, email } = payment;
   const data = qs.stringify({
-    amount: price,
+    amount: price || 99,
     currency: 'eur',
     'payment_method_types[]': type,
     'metadata[appointment_id]': appointmentId,
